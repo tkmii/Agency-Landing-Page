3e8d5a60dff22103e2ba4fe4158bfbffd9e2d7cc
Tatiana <tanya0fr@gmail.com>
2023-07-12 18:21:23 +0400

    Исправила баги с менюшкой

--- burger-menu.js
+++ burger-menu.js
@@ -1,4 +1,6 @@
+let burger = document.querySelector('.burger-menu');
 let mobileMenu = document.querySelector('.mobile-menu');
 document.addEventListener("click", function() {
+    burger.classList.toggle('animation-btn');
     mobileMenu.classList.toggle('open');
 });
