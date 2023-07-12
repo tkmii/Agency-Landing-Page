7a3e97caa3f009015dd1df180792c7a5c13f7734
Tatiana <tanya0fr@gmail.com>
2023-07-12 23:44:18 +0400

    Отредактировала скрипт

--- burger-menu.js
+++ burger-menu.js
@@ -1,6 +1,6 @@
 let burger = document.querySelector('.burger-menu');
 let mobileMenu = document.querySelector('.mobile-menu');
-document.addEventListener("click", function() {
+burger.addEventListener("click", function() {
     burger.classList.toggle('animation-btn');
     mobileMenu.classList.toggle('open');
 });
