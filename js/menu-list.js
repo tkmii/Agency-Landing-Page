cb390417bd86d35e3f5ddde6d6de834061adf749
Tatiana <tanya0fr@gmail.com>
2023-07-13 12:19:05 +0400

    Сделала выпадающий список в футере в мобильной версии

--- js/menu-list.js
+++ js/menu-list.js
@@ -1,8 +1,10 @@
 let menuList = document.querySelectorAll('.menu-btn');
+let menuLinks = document.querySelectorAll('.menu-links');
 
 for (let i = 0; i < menuList.length; ++i) {
     menuList[i].addEventListener("click", function() {
         menuList[i].classList.toggle('menu-btn-active');
+        menuLinks[i].classList.toggle('menu-links-active');
     });
 }
 /* menuList.*/
